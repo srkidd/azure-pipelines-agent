9df65f36d49077c07051fe88471e60c7878ba66c
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-08-21 23:52:16 +0400

    Add regex masking tests file

---
+++ src/Test/L0/SecretMaskerTests/RegexMaskingL0.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.TeamFoundation.DistributedTask.Logging;
+using Microsoft.VisualStudio.Services.Agent;
+using Microsoft.VisualStudio.Services.Agent.Worker;
+using Microsoft.VisualStudio.Services.Agent.Worker.Build;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test.L0.SecretMaskerTests
+{
+    internal class RegexMaskingL0
+{
+        private ISecretMasker initSecretMasker()
+        {
+            var testSecretMasker = new SecretMasker();
+            testSecretMasker.AddRegex(AdditionalMaskingRegexes.UrlSecretPattern);
+
+            return testSecretMasker;
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "SecretMasker")]
+        public void IsRegexHintMasked()
+        {
+            var testSecretMasker = initSecretMasker();
+
+            Assert.Equal(
+               "https://simpledomain@example.com",
+               testSecretMasker.MaskSecrets("https://simpledomain@example.com"));
+        }
+    }
+}
