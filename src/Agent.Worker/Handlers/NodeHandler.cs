a9c1270797b05ee44af0af46299653b306f05537
Denis Rumyantsev <mr.denis.rumyantsev@gmail.com>
2022-11-02 19:12:23 +0100

    fix code style

--- src/Agent.Worker/Handlers/NodeHandler.cs
+++ src/Agent.Worker/Handlers/NodeHandler.cs
@@ -179,7 +179,8 @@ namespace Microsoft.VisualStudio.Services.Agent.Worker.Handlers
             {
                 Trace.Info($"Detected RedHat 6, using node 10 as execution handler, instead node16");
                 nodeFolder = "node10";
-            } else if (taskHasNode16Data)
+            }
+            else if (taskHasNode16Data)
             {
                 Trace.Info($"Task.json has node16 handler data: {taskHasNode16Data}");
                 nodeFolder = "node16";
