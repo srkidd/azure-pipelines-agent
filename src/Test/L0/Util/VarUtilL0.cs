a9def1d6f043225277c33bb7d9ae00fce25fd0ac
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-11-22 13:59:14 +0400

    Add new test cases

--- src/Test/L0/Util/VarUtilL0.cs
+++ src/Test/L0/Util/VarUtilL0.cs
@@ -37,6 +37,27 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests.Util
         [Fact]
         [Trait("Level", "L0")]
         [Trait("Category", "Common")]
+        public void ReplacingToEnvVariablesForTaskMultiple()
+        {
+            using TestHostContext hc = new TestHostContext(this);
+
+            var source = new Dictionary<string, string>();
+
+            var target = new Dictionary<string, string>()
+            {
+                ["target1"] = "variable1 = $(system.DefinitionName) variable2 = $(build.DefinitionName)",
+                ["target2"] = "variable1 = $(system.stageDisplayName) variable2 = $(system.phaseDisplayName) variable3 = $(release.environmentName)",
+            };
+
+            VarUtil.ExpandValues(hc, source, target, "Bash");
+
+            Assert.Equal($"variable1 = $SYSTEM_DEFINITIONNAME variable2 = $BUILD_DEFINITIONNAME", target["target1"]);
+            Assert.Equal($"variable1 = $SYSTEM_STAGEDISPLAYNAME variable2 = $SYSTEM_PHASEDISPLAYNAME variable3 = $RELEASE_ENVIRONMENTNAME", target["target2"]);
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Common")]
         public void ReplaceVulnerableVariablesIgnoringCase()
         {
             using TestHostContext hc = new TestHostContext(this);
@@ -55,6 +76,85 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests.Util
             Assert.Equal("$SYSTEM_DEFINITIONNAME", target["system.DefinitionName var"]);
             Assert.Equal("$BUILD_DEFINITIONNAME", target["build.DefinitionName var"]);
             Assert.Equal("$BUILD_SOURCEVERSIONMESSAGE", target["build.SourceVersionMessage var"]);
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Common")]
+        public void CheckIfTargetNullOrEmpty()
+        {
+            using TestHostContext hc = new TestHostContext(this);
+
+            var source = new Dictionary<string, string>();
+
+            var target = new Dictionary<string, string>()
+            {
+                ["nullVar"] = null,
+                ["emptyVar"] = "",
+                ["spaceVar"] = " ",
+            };
+
+            VarUtil.ExpandValues(hc, source, target, "Bash");
+
+            Assert.Equal("", target["nullVar"]);
+            Assert.Equal("", target["emptyVar"]);
+            Assert.Equal(" ", target["spaceVar"]);
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Common")]
+        public void CheckIfSourceNullOrEmpty()
+        {
+            using TestHostContext hc = new TestHostContext(this);
+
+            var source = new Dictionary<string, string>()
+            {
+                ["nullSource"] = null,
+                ["emptySource"] = "",
+                ["spaceSource"] = " ",
+            };
+
+            var target = new Dictionary<string, string>()
+            {
+                ["nullTarget"] = "source = '$(nullSource)'",
+                ["emptyTarget"] = "source = '$(emptySource)'",
+                ["spaceTarget"] = "source = '$(spaceSource)'",
+            };
+
+            VarUtil.ExpandValues(hc, source, target);
+
+            Assert.Equal("source = ''", target["nullTarget"]);
+            Assert.Equal("source = ''", target["emptyTarget"]);
+            Assert.Equal("source = ' '", target["spaceTarget"]);
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Common")]
+        public void CheckIfMultipleVulnerableVariablesPresent()
+        {
+            using TestHostContext hc = new TestHostContext(this);
+
+            var source = new Dictionary<string, string>()
+            {
+                ["nullSource"] = null,
+                ["emptySource"] = "",
+                ["spaceSource"] = " ",
+            };
+
+            var target = new Dictionary<string, string>()
+            {
+                ["nullTarget"] = "source = '$(nullSource)'",
+                ["emptyTarget"] = "source = '$(emptySource)'",
+                ["spaceTarget"] = "source = '$(spaceSource)'",
+            };
+
+            VarUtil.ExpandValues(hc, source, target, "Bash");
+
+            Assert.Equal("source = ''", target["nullTarget"]);
+            Assert.Equal("source = ''", target["emptyTarget"]);
+            Assert.Equal("source = ' '", target["spaceTarget"]);
         }
 
         [Fact]
