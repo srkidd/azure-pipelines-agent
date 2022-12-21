443527e992898f8406fe4004a45c8407d7f43885
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2022-12-21 13:17:01 +0400

    Remove extra semicolon

--- src/Test/L0/Worker/VariablesL0.cs
+++ src/Test/L0/Worker/VariablesL0.cs
@@ -686,7 +686,7 @@ namespace Microsoft.VisualStudio.Services.Agent.Tests.Worker
                         return str.Replace("/path/to", "/another/path");
                     }
                     return str;
-                }; ;
+                };
 
                 Assert.Equal(0, warnings.Count);
 
