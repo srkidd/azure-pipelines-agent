7e0f0e8c6056a34a2dffaa8274cb82c0bd732020
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2023-05-16 01:03:37 +0400

    Add invoker params class

---
+++ src/Agent.Sdk/ProcessInvokerParams.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.TeamFoundation.Framework.Common;
+using Microsoft.VisualStudio.Services.Agent.Util;
+
+namespace Agent.Sdk
+{
+    public class ProcessInvokerParams
+    {
+        public string WorkingDirectory { get; init; } = null;
+
+        public string FileName { get; init; } = null;
+
+        public string Arguments { get; init; } = null;
+
+        public IDictionary<string, string> Environment { get; init; } = null;
+
+        public bool RequireExitCodeZero { get; init; } = false;
+
+        public Encoding OutputEncoding { get; init; } = null;
+
+        public bool KillProcessOnCancel { get; init; } = false;
+
+        public InputQueue<string> RedirectStandardIn { get; init; } = null;
+
+        public bool InheritConsoleHandler { get; init; } = false;
+
+        public bool KeepStandardInOpen { get; init; } = false;
+
+        public bool HighPriorityProcess { get; init; } = false;
+
+        public bool ContinueAfterCancelProcessTreeKillAttempt { get; init; } = ProcessInvoker.ContinueAfterCancelProcessTreeKillAttemptDefault;
+
+        public ProcessInvokerParams() { }
+
+        public ProcessInvokerParams(
+            string workingDirectory,
+            string fileName,
+            string arguments)
+        {
+            WorkingDirectory = workingDirectory;
+            FileName = fileName;
+            Arguments = arguments;
+        }
+    }
+}
