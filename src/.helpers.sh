9a4aae609ce150df87d7118ec29acb6831cea476
Konstantin Tyukalov <v-ktyukalov@microsoft.com>
2023-11-03 17:27:49 +0400

    Remove unused helper

--- src/.helpers.sh
+++ src/.helpers.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 
-function failed() {
+function failed()
+{
     local error=${1:-Undefined error}
     echo "Failed: $error" >&2
     popd
     exit 1
 }
 
-function warn() {
+function warn()
+{
     local error=${1:-Undefined error}
     echo "WARNING - FAILED: $error" >&2
 }
 
-function checkRC() {
+function checkRC()
+{
     local rc=$?
     if [ $rc -ne 0 ]; then
         failed "${1} Failed with return code $rc"
     fi
 }
 
-function heading() {
+function heading()
+{
     echo
     echo
     echo "-----------------------------------------"
     echo "  ${1}"
     echo "-----------------------------------------"
-}
-
-normalize_dir_path() {
-    dirToNormalize=${1}
-
-    normalizedDirPath=$(
-        cd "$dirToNormalize" || exit
-        pwd
-    )
-
-    echo "$normalizedDirPath"
 }
