5d6a32a1e29dc34c39dc61ada3992e57f31ed497
v-kivlev <undefined>
2023-12-12 15:34:49 +0400

    Add proper exception handling for Telemetery/FeatureFlag enpoints

--- src/Agent.Listener/Telemetry/TelemetryPublisher.cs
+++ src/Agent.Listener/Telemetry/TelemetryPublisher.cs
@@ -81,7 +81,15 @@ namespace Microsoft.VisualStudio.Services.Agent.Listener.Telemetry
 
                 using var vsConnection = VssUtil.CreateConnection(new Uri(settings.ServerUrl), creds, Trace, agentCertManager.SkipServerCertificateValidation);
                 _ciService.Initialize(vsConnection);
-                await PublishEventsAsync(context, ciEvent);
+                /// This endpoint is not accepting data on some old Azure DevOps OnPremise versions
+                try
+                {
+                    await PublishEventsAsync(context, ciEvent);
+                } catch (VssUnauthorizedException)
+                {
+                    Trace.Warning("Unable to publish telemetry data");
+                }
+                
 
             }
             // We never want to break pipelines in case of telemetry failure.
