94201af006a05110121ddd5fa6f2af6b1d3d4638
Ivan Duplenskikh <ivan.duplenskikh@akvelon.com>
2024-01-31 10:20:04 +0100

    Remove unnecessary

--- src/Agent.Sdk/Util/ArgUtil/ArgUtil.cs
+++ src/Agent.Sdk/Util/ArgUtil/ArgUtil.cs
@@ -1,11 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using Agent.Sdk.Util;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 namespace Microsoft.VisualStudio.Services.Agent.Util
 {
@@ -14,42 +11,42 @@ namespace Microsoft.VisualStudio.Services.Agent.Util
         public static IArgUtilInstanced ArgUtilInstance = new ArgUtilInstanced();
         public static void Directory([ValidatedNotNull] string directory, string name)
         {
-            ArgUtil.ArgUtilInstance.Directory(directory, name);
+            ArgUtilInstance.Directory(directory, name);
         }
 
         public static void Equal<T>(T expected, T actual, string name)
         {
-            ArgUtil.ArgUtilInstance.Equal(expected, actual, name);
+            ArgUtilInstance.Equal(expected, actual, name);
         }
 
         public static void File(string fileName, string name)
         {
-            ArgUtil.ArgUtilInstance.File(fileName, name);
+            ArgUtilInstance.File(fileName, name);
         }
 
         public static void NotNull([ValidatedNotNull] object value, string name)
         {
-            ArgUtil.ArgUtilInstance.NotNull(value, name);
+            ArgUtilInstance.NotNull(value, name);
         }
 
         public static void NotNullOrEmpty([ValidatedNotNull] string value, string name)
         {
-            ArgUtil.ArgUtilInstance.NotNullOrEmpty(value, name);
+            ArgUtilInstance.NotNullOrEmpty(value, name);
         }
 
         public static void ListNotNullOrEmpty<T>([ValidatedNotNull] IEnumerable<T> value, string name)
         {
-            ArgUtil.ArgUtilInstance.ListNotNullOrEmpty(value, name);
+            ArgUtilInstance.ListNotNullOrEmpty(value, name);
         }
 
         public static void NotEmpty(Guid value, string name)
         {
-            ArgUtil.ArgUtilInstance.NotEmpty(value, name);
+            ArgUtilInstance.NotEmpty(value, name);
         }
 
         public static void Null(object value, string name)
         {
-            ArgUtil.ArgUtilInstance.Null(value, name);
+            ArgUtilInstance.Null(value, name);
         }
     }
 
