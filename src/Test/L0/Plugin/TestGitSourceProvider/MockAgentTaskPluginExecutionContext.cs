3961c4ed9e9bcfb877b7bcfd41a18eda3cb510df
Ivan Duplenskikh <ivan.duplenskikh@akvelon.com>
2024-02-05 14:43:21 +0100

    Remove overrided method from mocked context

--- src/Test/L0/Plugin/TestGitSourceProvider/MockAgentTaskPluginExecutionContext.cs
+++ src/Test/L0/Plugin/TestGitSourceProvider/MockAgentTaskPluginExecutionContext.cs
@@ -1,13 +1,9 @@
 ﻿using Agent.Sdk;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Test.L0.Plugin.TestGitSourceProvider
 {
     public class MockAgentTaskPluginExecutionContext : AgentTaskPluginExecutionContext
     {
         public MockAgentTaskPluginExecutionContext(ITraceWriter trace) : base(trace) { }
-        public override void PrependPath(string directory) { }
     }
 }
