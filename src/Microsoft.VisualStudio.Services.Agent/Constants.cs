d87501e7f323cc60e5499464b6789601c33fb7db
Ivan Duplenskikh <ivan.duplenskikh@akvelon.com>
2024-02-06 10:09:51 +0100

    Fix variable value

--- src/Microsoft.VisualStudio.Services.Agent/Constants.cs
+++ src/Microsoft.VisualStudio.Services.Agent/Constants.cs
@@ -353,7 +353,7 @@ namespace Microsoft.VisualStudio.Services.Agent
                 public static readonly string TempDirectory = "agent.TempDirectory";
                 public static readonly string ToolsDirectory = "agent.ToolsDirectory";
                 public static readonly string UseGitLongPaths = "USE_GIT_LONG_PATHS";
-                public static readonly string UseGitSingleThread = "USE_GIT_LONG_PATHS";
+                public static readonly string UseGitSingleThread = "USE_GIT_SINGLE_THREAD";
                 public static readonly string UseLatestGitVersion = "USE_LATEST_GIT_VERSION";
                 public static readonly string Version = "agent.version";
                 public static readonly string WorkFolder = "agent.workfolder";
