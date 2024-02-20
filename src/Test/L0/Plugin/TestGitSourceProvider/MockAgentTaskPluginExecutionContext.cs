0905479bd175bf95fd508978e1b5318bbb2b40a8
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2024-02-20 19:52:33 +0100

    Update Git to v2.43.0 (#4618)
    * Update Git to a new version
    Add feature flag to use different Git versions in Worker and Listener
    Separate Git config and Git usage

---
+++ src/Test/L0/Plugin/TestGitSourceProvider/MockAgentTaskPluginExecutionContext.cs
@@ -0,0 +1,9 @@
+﻿using Agent.Sdk;
+
+namespace Test.L0.Plugin.TestGitSourceProvider
+{
+    public class MockAgentTaskPluginExecutionContext : AgentTaskPluginExecutionContext
+    {
+        public MockAgentTaskPluginExecutionContext(ITraceWriter trace) : base(trace) { }
+    }
+}
