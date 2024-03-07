736bc2f1466fa04331a02bec85babe8f9f596b31
MerlynOMsft <44985659+merlynomsft@users.noreply.github.com>
2024-03-07 08:17:36 -0800

    Implemented option to store token in named container (#4695)
    * Reapply "Implemented option to store token in named container (#4607)" (#4631)
    
    This reverts commit 612153e702f83ccb98a4308ed237e0843c2ac192.
    
    * fix for crash when ContainerName not present
    
    * Added CNG knob

--- src/Agent.Sdk/Knob/AgentKnobs.cs
+++ src/Agent.Sdk/Knob/AgentKnobs.cs
@@ -626,5 +626,17 @@ namespace Agent.Sdk.Knob
             "If true, agent will use fetch filter in checkout task.",
             new RuntimeKnobSource("AGENT_USE_FETCH_FILTER_IN_CHECKOUT_TASK"),
             new BuiltInDefaultKnobSource("false"));
+
+        public static readonly Knob StoreAgentKeyInCSPContainer = new Knob(
+            nameof(StoreAgentKeyInCSPContainer),
+            "Store agent key in named container (Windows).",
+            new EnvironmentKnobSource("STORE_AGENT_KEY_IN_CSP_CONTAINER"),
+            new BuiltInDefaultKnobSource("false"));
+
+        public static readonly Knob AgentKeyUseCng = new Knob(
+            nameof(AgentKeyUseCng),
+            "Use CNG API to store agent key.  Note: Uses Private User Storage",
+            new EnvironmentKnobSource("AGENT_KEY_USE_CNG"),
+            new BuiltInDefaultKnobSource("false"));
     }
 }
